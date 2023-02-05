0ed87cd7856ff830341e09e59cfb0c1b951e8ede
Charles Anikpe <charlesanikpe3@gmail.com>
2023-02-05 01:21:04 +0000

    Attempt of adding products dynamically to the home page

--- front/js/script.js
+++ front/js/script.js
@@ -3,8 +3,34 @@ fetch('http://localhost:3000/api/products')
     return data.json();
 })
 .then(products => {
-    console.log(products);
-});
+    console.log(products)
 
+
+let main = document.querySelector('main .limitedWidthBlock')
+console.log(main);
 let productsCard = document.getElementById('items');
 console.log(productsCard);
+
+main.appendChild(productsCard);
+
+
+for (product in products) {
+    let productLink = document.createElement('a');
+    productsCard.appendChild(productLink);
+    productLink.href = `./product.html?id=${products.id}`;
+    let productElement = document.createElement('article');
+    productsCard.appendChild(productElement);
+    let productImage = document.createElement('img');
+    productsCard.appendChild(productImage);
+    productImage.src = `${products.imageUrl}`;
+    productImage.alt = `${products.altTxt}`;
+    let productName = document.createElement('h3')
+    productsCard.appendChild(productName);
+    productName.setAttribute('class', 'productName');
+    productName.innerHTML = `${products.name}`
+
+        
+        
+    }
+});
+
