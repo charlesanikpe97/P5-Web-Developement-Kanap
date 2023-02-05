a31617124ca7d5093cf0a82a08bbb62bfb2ffe40
Charles Anikpe <charlesanikpe3@gmail.com>
2023-02-05 09:21:49 +0000

    Manage to add Kanap cards dynamically to the homepag e

--- front/js/script.js
+++ front/js/script.js
@@ -1,36 +1,41 @@
 fetch('http://localhost:3000/api/products')
-.then(data=> {
-    return data.json();
-})
-.then(products => {
-    console.log(products)
+    .then(data=> {
+        return data.json();
+    })
+    .then(products => {
+        console.log(products)
 
 
-let main = document.querySelector('main .limitedWidthBlock')
-console.log(main);
-let productsCard = document.getElementById('items');
-console.log(productsCard);
+    let main = document.querySelector('main .limitedWidthBlock')
+    console.log(main);
+    let productsCard = document.getElementById('items');
+    console.log(productsCard);
 
-main.appendChild(productsCard);
+    main.appendChild(productsCard);
 
 
-for (product in products) {
-    let productLink = document.createElement('a');
-    productsCard.appendChild(productLink);
-    productLink.href = `./product.html?id=${products.id}`;
-    let productElement = document.createElement('article');
-    productsCard.appendChild(productElement);
-    let productImage = document.createElement('img');
-    productsCard.appendChild(productImage);
-    productImage.src = `${products.imageUrl}`;
-    productImage.alt = `${products.altTxt}`;
-    let productName = document.createElement('h3')
-    productsCard.appendChild(productName);
-    productName.setAttribute('class', 'productName');
-    productName.innerHTML = `${products.name}`
-
+    for (let product of products) {
+        let productLink = document.createElement('a');
+        productsCard.appendChild(productLink);
+        productLink.href = `./product.html?id=${product._id}`;
+        let productElement = document.createElement('article');
+        productLink.appendChild(productElement);
+        let productImage = document.createElement('img');
+        productElement.appendChild(productImage);
+        productImage.src = product.imageUrl;
+        productImage.alt = product.altTxt;
+        let productName = document.createElement('h3')
+        productElement.appendChild(productName);
+        productName.setAttribute('class', 'productName');
+        productName.innerHTML = product.name;
+        let productDescription = document.createElement('p');
+        productElement.appendChild(productDescription);
+        productDescription.innerHTML = product.description;
+        productDescription.setAttribute('class', 'productDescription');
         
-        
-    }
-});
+
+            
+            
+        }
+    });
 
