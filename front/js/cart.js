d4fa1eee052185948e20889c9ca82a5bb5952036
Charles Anikpe <charlesanikpe3@gmail.com>
2023-02-09 00:22:49 +0000

    Fix some bugs preventing the order confirmation to display, changed cart html to fix the last name label made some changes to the confirmation.js file

--- front/js/cart.js
+++ front/js/cart.js
@@ -107,26 +107,23 @@ let totalQuantity = document.getElementById('totalQuantity');
 totalQuantity.innerHTML = articles;
 
 
-// let priceForEachProductInCart = [];
+let priceForEachProductInCart = [];
 let price = 0;
-// for (let i = 0; i < selectedProductsInCart.length; i++) {
-//     if (selectedProductsInCart[i].selectedQuantity == 0) { //This conditional statement helps prevent adding up the cost of products add to the cart with no quantity.
-//         selectedProductsInCart[i].selectedPrice = 0;
-//         priceForEachProductInCart.push(Number(selectedProductsInCart[i].selectedPrice));
+for (let i = 0; i < selectedProductsInCart.length; i++) {
+    if (selectedProductsInCart[i].selectedQuantity == 0) { //This conditional statement helps prevent adding up the cost of products add to the cart with no quantity.
+        selectedProductsInCart[i].selectedPrice = 0;
+        priceForEachProductInCart.push(Number(selectedProductsInCart[i].selectedPrice));
     
-//     } else {
-//         priceForEachProductInCart.push(Number(selectedProductsInCart[i].selectedPrice));
-//     }
+    } else {
+        priceForEachProductInCart.push(Number(selectedProductsInCart[i].selectedPrice));
+    }
    
     
-// }
+}
 
-// console.log(priceForEachProductInCart);
+console.log(priceForEachProductInCart);
 
-// for (let i = 0; i < priceForEachProductInCart.length; i++) {
-//     price += priceForEachProductInCart[i];
 
-// }
 
 for (let i = 0; i < quantityOfEachProductInCart.length; i++){
     price += (Number(quantityOfEachProductInCart[i]) * selectedProductsInCart[i].selectedPrice);
@@ -177,58 +174,57 @@ for (let i = 0; i < deleteButton.length; i++) {
         window.location.reload() //forces the cart page to reload and reflects changes made to the cart.
     })
     
-}
+  }
 
-let emailRegex = new RegExp(/^[A-Za-z0-9_!#$%&'*+\/=?`{|}~^.-]+@[A-Za-z0-9.-]+$/); //Email Validator Regular Expression
-let nameRegex = new RegExp(/\b([A-ZÀ-ÿ][-,a-z. ']+[ ]*)+/); //First name and Last Name Validator Regular Expression 
-let OnlyLettersRegex = new RegExp(/^[A-Za-z]+$/); // This could come handy for vaildating cities, as I believe they are no cities in the world that contains numbers in their name.
-let addressRegex = new RegExp(/^[a-zA-Z0-9 ]*$/);
+     //Form
 
+    // Variables Regex
+    let nameRegex = /^[a-zA-Z\-çñàéèêëïîôüù ]{2,}$/;
+    let addressRegex = /^[0-9a-zA-Z\s,.'-çñàéèêëïîôüù]{3,}$/;
+    let emailRegex = /^[A-Za-z0-9\-\.]+@([A-Za-z0-9\-]+\.)+[A-Za-z0-9-]{2,4}$/;
 
-let firstName = document.getElementById("firstName");
-let lastName = document.getElementById("lastName");
-let address = document.getElementById("address");
-let city = document.getElementById("city");
-let email = document.getElementById("email");
+// Variables to retrieve form field ids
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const address = document.getElementById("address");
+    const city = document.getElementById("city");
+    const email = document.getElementById("email");
 
-firstName.addEventListener("input", function(event){
+    firstName.addEventListener("input", (event) => {
     event.preventDefault();
     if (nameRegex.test(firstName.value) == false || firstName.value == "") {
     document.getElementById("firstNameErrorMsg").innerHTML =
       "Invalid first name";
-      return false;
   } else {
     document.getElementById("firstNameErrorMsg").innerHTML = "";
-    return true;
   }
 });
 
-lastName.addEventListener("input", function(event) {
+    lastName.addEventListener("input", (event) => {
     event.preventDefault();
     if (nameRegex.test(lastName.value) == false || lastName.value == "") {
-    document.getElementById("lastNameErrorMsg").innerHTML =
-      "Invalid last name";
-      return false;
-  } else {
+    document.getElementById("lastNameErrorMsg").innerHTML = "Invalid last name";
+    return false;
+    } else {
     document.getElementById("lastNameErrorMsg").innerHTML = "";
     return true;
   }
 });
 
-address.addEventListener("input", (event) => {
-    event.preventDefault();
-    if (addressRegex.test(address.value) == false || address.value == "") {
-      document.getElementById("addressErrorMsg").innerHTML = "Invalid Address";
-      return false;
-    } else {
-      document.getElementById("addressErrorMsg").innerHTML = "";
-      return true;
-    }
-  });
+    address.addEventListener("input", (event) => {
+  event.preventDefault();
+  if (addressRegex.test(address.value) == false || address.value == "") {
+    document.getElementById("addressErrorMsg").innerHTML = "Invalid Adress";
+    return false;
+  } else {
+    document.getElementById("addressErrorMsg").innerHTML = "";
+    return true;
+  }
+});
 
-city.addEventListener("input", function(event) {
+    city.addEventListener("input", (event) => {
     event.preventDefault();
-    if (OnlyLettersRegex.test(city.value) == false || city.value == "") {
+    if (nameRegex.test(city.value) == false || city.value == "") {
     document.getElementById("cityErrorMsg").innerHTML = "Invalid City";
     return false;
     } else {
@@ -237,7 +233,7 @@ city.addEventListener("input", function(event) {
 }
 });
 
-email.addEventListener("input", function(event) {
+    email.addEventListener("input", (event) => {
     event.preventDefault();
     if (emailRegex.test(email.value) == false || email.value == "") {
     document.getElementById("emailErrorMsg").innerHTML = "Invalid Email";
@@ -249,64 +245,63 @@ email.addEventListener("input", function(event) {
   }
 });
 
-let orderButton = document.getElementById('order'); // add onclick event listener to the order button;
-orderButton.addEventListener('click', function(event){
-    event.preventDefault;
-    let filledForm = {
-        firstName : firstName.value,
-        lastName : lastName.value,
-        address : address.value,
-        city : city.value,
-        email : email.value,
-    };
-    //Need to create conditional statements to capture empty form and failed vaildation through regex 
+    let order = document.getElementById("order");
+    order.addEventListener("click", function(event) {
+    event.preventDefault();
 
     if (
-        firstName.value === "" ||
-        lastName.value === "" ||
-        address.value === "" ||
-        city.value === "" ||
-        email.value === ""
-      ) {
-        window.confirm(
-          "You must enter your details to place the order!"
-        );
-      } else if (
-        nameRegex.test(firstName.value) == false ||
-        nameRegex.test(lastName.value) == false ||
-        addressRegex.test(address.value) == false ||
-        OnlyLettersRegex.test(city.value) == false ||
-        emailRegex.test(email.value) == false
-      ) {
-        window.confirm("Please enter your details correctly!");
-    } else {
-        let products = [];
-        selectedProductsInCart.forEach((selectedProductInCart) => {
-        products.push(selectedProductInCart.selectedId);
-        });
-    
+    firstName.value === "" ||
+    lastName.value === "" ||
+    address.value === "" ||
+    city.value === "" ||
+    email.value === ""
+  ) {
+    window.confirm(
+      "You must enter your details to place the order!"
+    );
+  } else if (
+    nameRegex.test(firstName.value) == false ||
+    nameRegex.test(lastName.value) == false ||
+    addressRegex.test(address.value) == false ||
+    nameRegex.test(city.value) == false ||
+    emailRegex.test(email.value) == false
+  ) {
+    window.confirm("Please enter your details correctly!");
+  } else {
 
-        let pageOrder ={filledForm, products};
-        fetch("http://localhost:3000/api/products/order", {
-            method: "POST",
-            headers: {
-            "Content-Type": "application/json",
-            },
-            body: JSON.stringify(pageOrder),
-            mode:"cors"
-        })
-            .then((res) => {
-            return res.json();
-            })
-            .then((confirm) => {
-            window.location.href = "./confirmation.html?orderId=" + confirm.orderId;
-            localStorage.clear();
-            })
-            .catch((error) => {
-            console.log("An error has occurred");
-            });
-        }
+    let contact = {
+      firstName: firstName.value,
+      lastName: lastName.value,
+      address: address.value,
+      city: city.value,
+      email: email.value,
+    };
+
+
+    let products = [];
+    selectedProductsInCart.forEach((order) => {
+    products.push(order.selectedId);
+    });
+
+    let pageOrder = {contact, products};
+
+    fetch("http://localhost:3000/api/products/order", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(pageOrder),
+      mode:"cors"
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((confirm) => {
+        window.location.href = "./confirmation.html?orderId=" + confirm.orderId;
+        localStorage.clear();
+      })
+      .catch((error) => {
+        console.log("An error has occurred");
+      });
+  }
 });
-
-
-
--- front/js/script.js
+++ front/js/script.js
@@ -1,6 +1,7 @@
 fetch('http://localhost:3000/api/products')
     .then(data=> {
         return data.json();
+        
     })
     .then(products => {
         console.log(products)
